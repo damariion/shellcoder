e63a34fdd2ca9cc52bdacbd45e18e714188896bb
Damarion Abendanon <da.abendanon@gmail.com>
2026-07-22 23:21:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+_Shellcoder_ is an assembler built specifically for use during the Offensive Security Exploit Developer (OSED) exam. It's extremely simple by design, converting assembly in text or file form into a Python buffer with helpful comments for debugging. Take, for example, the shellcode that resolves the base address of `kernel32.dll` by walking the [Process Environment Block](https://en.wikipedia.org/wiki/Process_Environment_Block) (`PEB`):
+
+```nasm
+getkrnl32:
+    push ebp
+    mov ebp, esp
+    sub esp, 0x20
+    xor edx, edx
+    mov dword ptr [ebp-0x04], esi
+    mov esi, fs:[edx+0x30]
+    mov esi, [esi+0x0C]
+    mov esi, [esi+0x1C]
+getkrnl32_next:
+    mov eax, [esi+0x08]
+    mov ecx, [esi+0x20]
+    mov esi, [esi]
+    cmp dword ptr [ecx+0x18], edx
+    jne getkrnl32_next
+getkrnl32_exit:
+    mov esi, [ebp-0x04]
+    mov esp, ebp
+    pop ebp
+    ret
+```
+
+Where `msfvenom` gives you a dump of bytes that aren't easily modifiable individually, this tool displays the shellcode on a per-line basis, with its original assembly beside the bytecode. It also highlights bad characters in the terminal and marks any affected line with `<!>`, so you can find it easily in a text editor, where colours don't apply.
+
+![example](./assets/example.png)
---
+++ shellcoder.py
@@ -0,0 +1,154 @@
+from capstone import Cs, CS_ARCH_X86, CS_MODE_32, CsInsn
+from keystone import Ks, KS_ARCH_X86, KS_MODE_32
+from argparse import ArgumentParser
+
+"""
+* Keystone and Capstone haven't been properly type-annotated, which results
+* in many errors when Pylance is configured in strict-mode. The wrappers
+* below are meant to make the rest of the source code more readable.
+"""
+def ks_asm(ks: Ks, content: str) -> bytes       : return ks.asm(content, 0)[0]
+def cs_dis(cs: Cs, code: bytes) -> list[CsInsn] : return [*cs.disasm(code, 0)]
+
+def parse_arguments() -> tuple[str, str, list[int]] | None:
+    """
+    parse and validate the CLI-arguments.<br>
+    **returns**: <contents\\>, <name\\>, <characters\\>
+    """
+
+    # ? parse through the built-in argument handler
+    parser = ArgumentParser()
+    parser.add_argument('-f', "--file", required=False,
+        action="store", help="assemble contents of a file")
+    parser.add_argument('-t', "--text", required=False,
+        action="store", help="assemble contents of a text")
+    parser.add_argument('-b', "--bads", required=False, default="0",
+        action="store", help="bad-characters to highlight (hex)")
+    parser.add_argument('-n', "--name", required=False, default="buffer",
+        action="store", help="name of the output buffer")
+    params = parser.parse_args().__dict__
+
+    # ? ensure at least one source for content
+    if not any([params["text"], params["file"]]):
+        return print("No contents provided.")
+    
+    if all([params["text"], params["file"]]):
+        return print("Too many contents provided.")
+
+    # ? convert bad-characters (if exist) to integers
+    characters: list[int] = []
+    for char in params["bads"].split(','):
+
+        if not char: 
+            continue
+
+        try:
+            if not (0 <= (xchar := int(char, 16)) <= 0xFF):
+                raise OverflowError
+            characters.append(xchar)
+
+        except ValueError:
+            return print(f"Invalid bad-character: '{char}' (invalid hex)")
+        except OverflowError:
+            return print(f"Invalid bad-character: '{char}' (range: 0-FF)")
+    
+    # ? read contents from file if path is provided
+    if not params["file"]: 
+
+        return params["text"], \
+               params["name"], \
+               characters
+    
+    try:
+
+        with open(params["file"], 'r') as file:
+            # remove comments and newlines to match Keystone syntax
+            nocoms = [x.split(';')[0].strip() for x in file.readlines()]
+            return ';'.join(nocoms), params["name"], characters
+
+    except PermissionError:
+        return print("Couldn't open file (insufficient permissions)")
+    except FileNotFoundError:
+        return print("Couldn't open file (non-existent path)")
+
+def convert_to_CR(content: str) -> list[CsInsn] | None:
+    """
+    convert the assembly-contents to an common representation (CsInsn)<br>
+    **returns**: list<capstone.CsInsn\\>
+    """
+
+    ks = Ks(KS_ARCH_X86, KS_MODE_32)
+    cs = Cs(CS_ARCH_X86, CS_MODE_32)
+
+    # ? pass through both KS and CS to reach CR (aka: CsInsn)
+    try:
+
+        code = ks_asm(ks, content)
+        text = cs_dis(cs, bytes(code))
+
+        # prevents Cs from ignoring instructions silently
+        if sum(len(i.bytes) for i in text) != len(code):
+            return print("Couldn't assemble all instructions")
+
+        return text
+    
+    except Exception as e:
+        return print(f"Couldn't convert to IR: {e}")
+
+def create_lines(cr: list[CsInsn], name: str) -> list[str]:
+    """
+    generate every line that will eventually be output, these lines<br>
+    follow the format: <name\\> += b"<bytes\\>" # <offset\\> | <text\\><br>
+    **returns**: list<lines\\>
+    """
+
+    # metadata required for neat alignment
+    align_com = max(cr, key=lambda x: len(x.bytes))
+    align_com = len(align_com.bytes) * 4 + 7
+    align_num = len(f"{cr[-1].address:x}") + 1
+
+    # ? construct the output-string (without any highlights)
+    lines: list[str] = []
+    for line in cr:
+
+        # <name> += b"<bytes>" # <offset> | <text>
+        lines.append('%s += b"\\x%s"%s#%s%s | %s' \
+            % (
+                name,
+                '\\x'.join([f'{n:02X}' for n in line.bytes]),
+                ' ' * (align_com - (len(line.bytes) * 4 + 6)),
+                ' ' * (align_num - len(f"{line.address:x}")),
+                f"{line.address:x}",
+                f"{line.mnemonic} {line.op_str}"
+            ))   
+
+    return lines
+
+def to_stdout(lines: list[str], name: str, chars: list[int]) -> None:
+    "display all lines on the terminal in a colour-coded manner"
+
+    characters: list[str] = [f"\\x{n:02X}" for n in chars]
+
+    # ? insert the highlighting of bad-characters
+    for index, line in enumerate(lines):
+
+        # proceed if line does not contain bad-characters
+        if not any([char in line for char in characters]):
+            continue
+
+        # colour mnemonical instruction
+        edge = line.find("|") + 1
+        lines[index] = line[:edge] + "\033[31m" + \
+                       line[edge:] + "\033[0m " + '<!>'
+
+    # colour individual instances
+    output: str = '\n'.join(lines)
+    for char in characters:
+        output = output.replace(char, f"\033[31m{char}\033[0m")
+
+    print(f"{name}  = b''\n" + output)
+
+if __name__ == "__main__" and (args := parse_arguments()):
+
+    if (cr := convert_to_CR(args[0])):
+        to_stdout(create_lines(cr, args[1]), *args[1:])
